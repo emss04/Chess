fa1724a550afe25d9751b0194f066e21db464e73
Kursedachilles <emss2004@gmail.com>
2024-11-15 00:54:00 -0500

    Piece/Board additions
    1. Added bishop move logic along with other piece starter methods
    2. Added board and controller initialization and reset method for board
    3. Added main class

---
+++ ChessGame/Chess/Controller.py
@@ -0,0 +1,8 @@
+from Board import Board
+
+class Controller:
+    def __init__(self):
+        self.board = Board()
+        self.board.reset()
+
+
---
+++ ChessGame/Chess/main.py
@@ -0,0 +1,8 @@
+from Controller import Controller
+
+
+def main():
+    controller = Controller()
+    print(controller.board.board)
+if __name__ == "__main__":
+    main()
